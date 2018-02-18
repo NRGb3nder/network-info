42b361de7979b4dfd1083c071ae81eeb508f5941
NRGb3nder <energybender120@gmail.com>
2018-02-18 20:53:09 +0300

    Add main routine

---
+++ networkinfo.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "adaptersmeta.h"
+#include "netresources.h"
+
+int main()
+{
+    LPNETRESOURCE lpnr = NULL;
+    struct AdapterMetaListNode *pAdapterList;
+    struct AdapterMetaListNode *pCurrentNode;
+
+    pAdapterList = (struct AdapterMetaListNode *) malloc(sizeof(struct AdapterMetaListNode));
+    pAdapterList->pInfo = NULL;
+    pAdapterList->pNext = NULL;
+
+    if (!getAdaptersMetaInfo(pAdapterList)) {
+        printf("Can not get adapters info\n");
+    } else {
+        printf("MAC ADDRESSES:\n");
+        pCurrentNode = pAdapterList;
+        while (pCurrentNode->pNext) {
+            pCurrentNode = pCurrentNode->pNext;
+            printf("\n%s\n%s\n", pCurrentNode->pInfo->szDesc,
+                pCurrentNode->pInfo->szAddress);
+        }
+        printf("\n");
+    }
+
+    printf("NETWORK RESOURCES:\n\n");
+    if (enumerateResources(lpnr) == false) {
+        printf("Call to EnumerateFunc failed");
+    }
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
